e65a6de0db3a43fc14913e9f50c3cf35e28e6529
Nikita Aniperko <nikan0ne@mail.ru>
2024-01-11 12:56:32 +0300

    HW1/ThirdBranch was created and changed

--- app/src/main/java/com/example/andersenlesson1/MainActivity.kt
+++ app/src/main/java/com/example/andersenlesson1/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android Welcome to second branch")
+                    Greeting("Android Welcome to second branch and now welcome to third!")
                 }
             }
         }
